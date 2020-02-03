1d6487c1f8d577c15ab96af8a0e68a35b349d038
Dansbo <erik@dansbo.dk>
2020-02-03 02:00:13 +0100

    Made sprite move

--- sprite.asm
+++ sprite.asm
@@ -2,16 +2,48 @@
 !byte $0C,$08,$0A,$00,$9E,' ','2','0','6','4',$00,$00,$00
 *=$0810
 
-VERA_CTRL = $9F25
-VERA_LOW = $9F20
-VERA_MID = $9F21
-VERA_HI = $9F22
-VERA_DATA = $9F23
+VERA_CTRL       = $9F25
+VERA_LOW        = $9F20
+VERA_MID        = $9F21
+VERA_HI         = $9F22
+VERA_DATA       = $9F23
+POS            = $00
+JIFFIES         = $02
+SETTIM		= $FFDB
+RDTIM		= $FFDE
 
         jsr Load
         jsr Enable
+        jsr Move
 
         rts             ;End program
+
+Delay:  lda #0
+        jsr SETTIM
+@Delay  jsr RDTIM
+        cmp JIFFIES
+        bne @Delay
+        rts
+
+
+
+Move:   lda #$2F
+        sta VERA_HI
+@Move:  inc POS
+        lda POS
+        cmp #$FF
+        beq @End
+        lda #2
+        sta VERA_LOW
+        lda POS
+        sta VERA_DATA
+        sta VERA_DATA
+        lda #1
+        sta JIFFIES
+        jsr Delay
+        jmp @Move
+
+@End    rts
 
 ;************************************************************************
 ;Enable sprite
